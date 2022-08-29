4e64f38aaf79572455361b683b161936c3fa4e6b
qiaozhen <qiaozhenleve@163.com>
2022-08-29 10:21:40 +0800

    flx: 添加基础配置

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
-  <router-view v-slot="{ Component }">
+  <router-view v-slot="{ Component, route }">
     <keep-alive>
-      <component :is="Component" v-if="$route.meta.keepAlive" />
+      <component :is="Component" :key="route.name" v-if="route.meta.keepAlive" />
     </keep-alive>
-    <component :is="Component" v-if="!$route.meta.keepAlive" />
+    <component :is="Component" :key="route.name" v-if="!route.meta.keepAlive" />
   </router-view>
 </template>
 
+<script setup lang="ts"></script>
 
-<script setup lang="ts">
-
-</script>
-
-<style scoped>
-</style>
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig, loadEnv } from "vite";
+import AutoImport from 'unplugin-auto-import/vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import viteCompression from 'vite-plugin-compression';
 import vue from "@vitejs/plugin-vue";
 import path from 'path'
 
 const config = loadEnv('development', './')
 
 export default defineConfig({
-  plugins: [vue()],
+
   server: {
     https: false,
     host: '0.0.0.0',
-    port: 8000,
+    port: parseInt(config.VITE_PORT),
     proxy: {
       '/api': {
         target: config.VITE_REQUEST_URL,
@@ -32,4 +35,25 @@ export default defineConfig({
       },
     },
   },
+  plugins: [
+    vue(),
+    //  hooks 自动引入
+    AutoImport({
+      imports: ['vue', 'vue-router', 'vuex', '@vueuse/head'],
+      dts: 'src/auto-import.d.ts'
+    }),
+    // 对 index.html 注入动态数据
+    createHtmlPlugin({
+      minify: true,
+      entry: '/src/main.ts',
+      inject: {
+        data: {
+          title: config.VITE_GLOB_APP_TITLE,
+          injectScript: `<script src="/inject.js"></script>`,
+        },
+      },
+    }),
+    // 使用 gzip 或者 brotli 来压缩资源
+    viteCompression()
+  ],
 });
