026dff27ef34d8910011825f007c7c2c51b1354a
qiaozhen <qiaozhenleve@163.com>
2022-09-01 11:51:32 +0800

    flx

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <el-config-provider :message="config.max" :size="config.size" :autoInsertSpace="config.autoInsertSpace">
     <router-view v-slot="{ Component, route }">
       <keep-alive>
-        <component :is="Component" :key="route.name" v-if="route.meta.keepAlive" />
+        <component :is="Component" :name="route.name" :key="route.name" v-if="route.meta.keepAlive" />
       </keep-alive>
-      <component :is="Component" :key="route.name" v-if="!route.meta.keepAlive" />
+      <component :is="Component" :name="route.name" :key="route.name" v-if="!route.meta.keepAlive" />
     </router-view>
   </el-config-provider>
 </template>
